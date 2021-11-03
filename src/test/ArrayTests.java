5721c1fb98fbcfb04d74cb2871ec73d380d71bf0
JD <j.davis2344@student.leedsbeckett.ac.uk>
2021-11-03 18:39:55 +0000

    Added initial array tests

--- src/test/ArrayTests.java
+++ src/test/ArrayTests.java
@@ -1,5 +1,22 @@
 package test;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import json.JSONArray;
+import json.JSONFactory;
+
 public class ArrayTests {
 
+	@Test
+	public void testArrayInits() {
+		JSONArray firstA = JSONFactory.createArray();
+		JSONArray secondA = JSONFactory.createArray(10);
+		JSONArray lastA = JSONFactory.createArray(secondA);
+		
+		assertTrue(firstA instanceof JSONArray);
+		assertTrue(secondA instanceof JSONArray);
+		assertTrue(lastA instanceof JSONArray);
+	}
 }
