b895057d454bd11fb33c877c42392295c2bb80c4
JD <j.davis2344@student.leedsbeckett.ac.uk>
2021-11-03 16:24:52 +0000

    Added some functionality to the ParserTests class.

--- src/test/ParserTests.java
+++ src/test/ParserTests.java
@@ -1,5 +1,37 @@
 package test;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.junit.jupiter.api.Test;
+
+import json.JSONException;
+import json.JSONParser;
+import json.JSONValue;
+
+/**
+ * Tests functionality of the {@link JSONParser} class
+ * 
+ * @author James Davis - c3576413
+ *
+ */
 public class ParserTests {
-	
+
+	@Test
+	public void testParsers() {
+		try {
+			JSONValue firstParsed = JSONParser.parse("");
+
+			JSONValue secondParsed = JSONParser.parse(new StringReader("{}"));
+
+			JSONValue thirdParsed = JSONParser.parseFile("jsonFiles/Testing.json");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
