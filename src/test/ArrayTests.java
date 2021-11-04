f6b63696e3da559b46f12019e4240bb3972130e3
JD <j.davis2344@student.leedsbeckett.ac.uk>
2021-11-04 00:13:21 +0000

    Added some tests for booleans

--- src/test/ArrayTests.java
+++ src/test/ArrayTests.java
@@ -1,5 +1,6 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -7,16 +8,44 @@ import org.junit.jupiter.api.Test;
 import json.JSONArray;
 import json.JSONFactory;
 
+/**
+ * Tests all functionality of the {@link JSONArray} class
+ * @author James Davis - c3576413
+ *
+ */
 public class ArrayTests {
-
+	
+	/**
+	 * Tests the constructors of the {@link JSONArray} class
+	 */
 	@Test
 	public void testArrayInits() {
 		JSONArray firstA = JSONFactory.createArray();
 		JSONArray secondA = JSONFactory.createArray(10);
+		secondA.addValue(false);
 		JSONArray lastA = JSONFactory.createArray(secondA);
 		
 		assertTrue(firstA instanceof JSONArray);
 		assertTrue(secondA instanceof JSONArray);
 		assertTrue(lastA instanceof JSONArray);
+	}
+	
+	/**
+	 * Tests some of the basic functions of the {@link JSONArray} class
+	 */
+	@Test
+	public void testArrayMethods() {
+		JSONArray firstA = JSONFactory.createArray();
+		JSONArray copyA = firstA.copy();
+		JSONArray thirdA = JSONFactory.createArray();
+		thirdA.addValue(false);
+		
+		assertTrue(firstA.isArray());
+		assertEquals(firstA, copyA);
+		assertTrue(firstA.equals(copyA));
+		assertEquals(firstA.hashCode(), copyA.hashCode());
+		
+		assertEquals(firstA.toString(), "[]");
+		assertEquals(thirdA.toString(), "[false]");
 	}
 }
--- src/test/BooleanTests.java
+++ src/test/BooleanTests.java
@@ -1,5 +1,37 @@
 package test;
 
-public class BooleanTests {
+import static org.junit.Assert.assertTrue;
 
+import org.junit.jupiter.api.Test;
+
+import json.JSONBoolean;
+import json.JSONFactory;
+
+/**
+ * Tests functionality of the {@link JSONBoolean} class
+ * @author James Davis - c3576413
+ *
+ */
+public class BooleanTests {
+	
+	/**
+	 * Tests the {@link JSONBoolean} constructor
+	 */
+	@Test
+	public void testBooleanInit() {
+		JSONBoolean firstVal = JSONFactory.createBoolean(false);
+		
+		assertTrue(firstVal instanceof JSONBoolean);
+	}
+	
+	/**
+	 * Checks that the toString() Method works
+	 */
+	@Test
+	public void testToString() {
+		JSONBoolean firstVal = JSONFactory.createBoolean(false);
+		JSONBoolean secondVal = JSONFactory.createBoolean(true);
+		assertTrue(firstVal.toString() == "false");
+		assertTrue(secondVal.toString() == "true");
+	}
 }
