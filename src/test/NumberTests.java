5a472082c85699a473bfe205be7923510a10298e
JD2344 <74184849+JD2344@users.noreply.github.com>
2021-11-05 10:24:55 +0000

    Added JSONNumber Test

--- src/test/NumberTests.java
+++ src/test/NumberTests.java
@@ -1,5 +1,23 @@
 package test;
 
+import org.junit.jupiter.api.Test;
+
+import json.JSONFactory;
+import json.JSONNumber;
+
+/**
+ * Tests the {@link JSONNumber} class functionality
+ * @author James Davis - c3576413
+ *
+ */
 public class NumberTests {
 
+	@Test
+	public void testConstructors() {
+		JSONNumber withDouble = JSONFactory.createNumber(10.2);
+		JSONNumber withFloat = JSONFactory.createNumber(22.4f);
+		JSONNumber withLong = JSONFactory.createNumber(213425245l);
+		
+		
+	}
 }
