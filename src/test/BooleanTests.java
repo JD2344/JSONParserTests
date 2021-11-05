310762608046c7aaf8561c4dec8eb163b18e05ef
JD2344 <74184849+JD2344@users.noreply.github.com>
2021-11-05 09:59:27 +0000

    Added tests externally
    Added some additional testing functionality to file.

--- src/test/BooleanTests.java
+++ src/test/BooleanTests.java
@@ -37,4 +37,33 @@ public class BooleanTests {
 		assertEquals(secondVal.toString(), "true");
 	}
 	
+	/**
+	 * Provides test for {@link JSONBoolean} asBoolean()
+	 */
+	@Test
+	public void testAsBoolean() {
+		JSONBoolean testVal = JSONFactory.createBoolean(false);
+		assertTrue(testVal.asBoolean() == false);
+	}
+	
+	/**
+	 * Test the {@link JSONBoolean} hashcode functionality
+	 */
+	@Test
+	public void testHashCodes() {
+		JSONBoolean firstBool = JSONFactory.createBoolean(true);
+		JSONBoolean secondBool = JSONFactory.createBoolean(false);
+		
+		assertTrue(firstBool.hashCode() == 1231);
+		assertTrue(secondBool.hashCode() == 1237);
+	}
+	
+	/**
+	 * Test the {@link JSONBoolean} equals method
+	 */
+	@Test
+	public void testEquals() {
+		JSONBoolean bool = JSONFactory.createBoolean(false);
+		
+	}
 }
