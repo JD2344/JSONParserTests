aeb6fb88b70aadf0e27ef6bd94697d3bad5a53c1
JD <j.davis2344@student.leedsbeckett.ac.uk>
2021-11-09 17:49:13 +0000

    Added test to ObjectTests

--- src/test/ObjectTests.java
+++ src/test/ObjectTests.java
@@ -1,5 +1,28 @@
 package test;
 
-public class ObjectTests {
+import static org.junit.Assert.assertTrue;
 
+import org.junit.jupiter.api.Test;
+
+import json.JSONFactory;
+import json.JSONObject;
+
+/**
+ * Provides tests for the {@link JSONObject} class
+ * @author james
+ *
+ */
+public class ObjectTests {
+	
+	/**
+	 * Tests the constructors for the {@link JSONObject} class
+	 */
+	@Test
+	public void testConstructors() {
+		JSONObject firstObj = JSONFactory.createObject();
+		JSONObject secondObj = JSONFactory.createObject(firstObj);
+		
+		assertTrue(firstObj instanceof JSONObject);
+		assertTrue(secondObj instanceof JSONObject);
+	}
 }
