c17b4216c07783ed6dec33edbcd595a94555389d
JD <j.davis2344@student.leedsbeckett.ac.uk>
2021-11-16 15:57:05 +0000

    added number tests

--- src/test/NumberTests.java
+++ src/test/NumberTests.java
@@ -1,5 +1,8 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 import json.JSONFactory;
@@ -18,36 +21,52 @@ public class NumberTests {
 		JSONNumber withFloat = JSONFactory.createNumber(22.412312343f);
 		JSONNumber withLong = JSONFactory.createNumber(213425245l);
 		
-		
+		assertTrue(withDouble instanceof JSONNumber);
+		assertTrue(withFloat instanceof JSONNumber);
+		assertTrue(withLong instanceof JSONNumber);
 	}
 	
 	@Test
 	public void testEquals() {
+		JSONNumber firstNum = JSONFactory.createNumber(10.2);
+		JSONNumber firstNumCopy = JSONFactory.createNumber(10.2);
 		
+		assertTrue(firstNum.equals(firstNumCopy));
+		assertEquals(firstNum.equals(null), false);
 	}
 	
 	@Test
 	public void testHashcode() {
-		
+		JSONNumber testVal = JSONFactory.createNumber(10.21f);
+		JSONNumber testValCopy = JSONFactory.createNumber(10.21f);
+		assertEquals(testVal.hashCode(), testValCopy.hashCode());
 	}
 	
 	@Test
 	public void testToString() {
-		
+		JSONNumber numberVal = JSONFactory.createNumber(1234567890);
+		assertTrue(numberVal.toString() instanceof String);
+		assertEquals(numberVal.toString(), "1234567890");
 	}
 	
 	@Test
 	public void testGetNumber() {
-		
+		JSONNumber nVal = JSONFactory.createNumber(10.3);
+		assertTrue(nVal.getNumber() == (Object)10.3);
 	}
 	
 	@Test
 	public void testIsNumber() {
-		
+		JSONNumber nVal = JSONFactory.createNumber(10.3);
+		assertTrue(nVal.isNumber());
 	}
 	
 	@Test
 	public void testAsType() {
+		JSONNumber withInt = JSONFactory.createNumber(1);
+		JSONNumber withDouble = JSONFactory.createNumber(10.2);
+		JSONNumber withFloat = JSONFactory.createNumber(22.412312343f);
+		JSONNumber withLong = JSONFactory.createNumber(213425245l);
 		
 	}
 }
