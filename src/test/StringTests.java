4e08086567c6844153e1b548108240766dda4d6d
JD <j.davis2344@student.leedsbeckett.ac.uk>
2021-11-18 00:33:39 +0000

    Added more string tests

--- src/test/StringTests.java
+++ src/test/StringTests.java
@@ -1,5 +1,34 @@
 package test;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import json.JSONException;
+import json.JSONFactory;
+import json.JSONString;
+
+/**
+ * Tests the functionality of the {@link JSONString} class
+ * 
+ * @author James Davis - c3576413
+ *
+ */
 public class StringTests {
 
+	@Test
+	public void testConstructor() {
+		JSONString s = JSONFactory.createString("testString");
+		assertTrue(s instanceof JSONString);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { "\"", "\\", "/" })
+	public void testEncoder(String testVal) {
+		JSONString test = JSONFactory.createString(testVal);
+		System.out.println(test.toString() + " " + testVal);
+		assertTrue(test.toString() == testVal);
+	}
 }
