f7666ced26c2bba5d970c628d5e65e69595ab68d
JD <j.davis2344@student.leedsbeckett.ac.uk>
2021-11-18 00:06:14 +0000

    Added testing of JSONExceptions

--- src/test/ExceptionTests.java
+++ src/test/ExceptionTests.java
@@ -1,5 +1,55 @@
 package test;
 
+import static org.junit.Assert.assertThrows;
+
+import java.io.IOException;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import json.JSONException;
+import json.JSONParser;
+import json.JSONValue;
+
+/**
+ * Tests various exception related functionality
+ * 
+ * @author James Davis - c3576413
+ *
+ */
 public class ExceptionTests {
 
+	@ParameterizedTest
+	@ValueSource(strings = { "\"key\"", "{}", "[]", "true", "10.2", 
+			"22.4434343f", "2139292929", "1" })
+	public void testJSONValueasObject(String testVal) {
+		JSONValue val;
+		try {
+			val = JSONParser.parse(testVal);
+			if(!val.isObject()) {
+				assertThrows(JSONException.class, () -> val.asObject());				
+			}
+			if(!val.isString()) {
+				assertThrows(JSONException.class, () -> val.asString());
+			}
+			assertThrows(JSONException.class, () -> val.asByte());
+			assertThrows(JSONException.class, () -> val.asShort());
+			assertThrows(JSONException.class, () -> val.asInteger());
+			assertThrows(JSONException.class, () -> val.asLong());
+			assertThrows(JSONException.class, () -> val.asFloat());
+			assertThrows(JSONException.class, () -> val.asDouble());
+			if(!val.isBoolean()) {
+				assertThrows(JSONException.class, () -> val.asBoolean());				
+			}
+			if(!val.isArray()) {
+				assertThrows(JSONException.class, () -> val.asArray());				
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
